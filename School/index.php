e232e3684942c60c79d18cfc2d7c6e63073d6221
Yuki-M612 <spear0128@gmail.com>
2025-06-09 09:54:28 +0800

    change designs

--- School/index.php
+++ School/index.php
@@ -7,7 +7,7 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" />
 
 </head>
-<body class="bg-white">
+<body class="bg-primary">
   <div class="container mt-5 mx-auto" style="max-width: 400px;">
     <!-- タイトル -->
     <h1 class="text-center mb-4">REGISTRATION</h1>
